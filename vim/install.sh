d62acfb416028d18ab0ee524171e13ebf47a62a7
Youngseok <youngseok.dev@gmail.com>
2022-07-23 14:15:10 +0900

    Update install.sh

--- vim/install.sh
+++ vim/install.sh
@@ -3,6 +3,6 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 git clone https://github.com/whonore/Coqtail.git ~/.vim/bundle/Coqtail
 vim +Helptags +q
 git clone https://github.com/jnurmine/Zenburn.git ~/.vim/bundle/Zemburn
-git submodule add git@github.com:dracula/vim.git ~/.vim/bundle/dracula
+git clone https://github.com/dracula/vim.git ~/.vim/bundle/dracula
 git clone git://github.com/altercation/vim-colors-solarized.git ~/.vim/bundle/vim-colors-solarized
 git clone git://github.com/wakatime/vim-wakatime.git ~/.vim/bundle/vim-wakatime
