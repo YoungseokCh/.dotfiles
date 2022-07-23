b723dd4986e87c74c33da21b539991db7619fbdd
Youngseok <youngseok.dev@gmail.com>
2022-07-23 14:39:10 +0900

    Update install.sh

--- vim/install.sh
+++ vim/install.sh
@@ -4,5 +4,5 @@ git clone https://github.com/whonore/Coqtail.git ~/.vim/bundle/Coqtail
 vim +Helptags +q
 git clone https://github.com/jnurmine/Zenburn.git ~/.vim/bundle/Zemburn
 git clone https://github.com/dracula/vim.git ~/.vim/bundle/dracula
-git clone git://github.com/altercation/vim-colors-solarized.git ~/.vim/bundle/vim-colors-solarized
-git clone git://github.com/wakatime/vim-wakatime.git ~/.vim/bundle/vim-wakatime
+git clone https://github.com/altercation/vim-colors-solarized.git ~/.vim/bundle/vim-colors-solarized
+git clone https://github.com/wakatime/vim-wakatime.git ~/.vim/bundle/vim-wakatime
