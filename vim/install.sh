545f9fc9304870489123971dfc62174283b0ffdc
Youngseok Choi <youngseok.choi@kaist.ac.kr>
2022-08-04 16:09:30 +0900

    Add latex unicoder plugin to vim install script

--- vim/install.sh
+++ vim/install.sh
@@ -6,3 +6,4 @@ git clone https://github.com/jnurmine/Zenburn.git ~/.vim/bundle/Zemburn
 git clone https://github.com/dracula/vim.git ~/.vim/bundle/dracula
 git clone https://github.com/altercation/vim-colors-solarized.git ~/.vim/bundle/vim-colors-solarized
 git clone https://github.com/wakatime/vim-wakatime.git ~/.vim/bundle/vim-wakatime
+git clone https://github.com/joom/latex-unicoder.vim.git ~/.vim/bundle/latex-unicoder.vim
