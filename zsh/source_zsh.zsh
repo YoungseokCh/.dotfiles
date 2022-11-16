2d01c7b5cfd214379f3a5bef2b4c8c9008e9185f
Youngseok Choi <youngseok.choi@kaist.ac.kr>
2022-11-16 05:28:48 +0000

    Update zsh

--- zsh/source_zsh.zsh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/zsh
-DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-ln -sf "$DOTFILES_DIR/zshrc" $HOME/.zshrc
-source $HOME/.zshrc
--- zsh/zsh_install.sh
+++ zsh/zsh_install.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 sudo apt install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+ln -sf "$DOTFILES_DIR/zshrc" $HOME/.zshrc
+source $HOME/.zshrc
