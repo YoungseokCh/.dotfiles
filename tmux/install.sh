5bb9975e6fae96a9878551ec871abb0b6ea0a6aa
Youngseok Choi <youngseok.choi@kaist.ac.kr>
2022-12-09 11:37:07 +0900

    Update tmux configuration to use Dracula

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,2 +1,19 @@
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+#!/bin/sh
+
+if ! type tmux &> /dev/null; then
+  sudo apt install tmux
+fi
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [ ! -e "$HOME/.tmux.conf" ] ; then
+  ln -sf "$DIR/tmux.conf" $HOME/.tmux.conf
+fi
+
+if [ ! -d "$HOME/.tmux/plugins/tpm" ] ; then
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+
+
+echo "Type prefix + I to apply plugins"
 
