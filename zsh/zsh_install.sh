f747256ce67aa0ce53066e73aa4d5149bb1bbff9
Youngseok Choi <youngseok.choi@kaist.ac.kr>
2022-12-29 17:24:25 +0900

    Update zsh_isntall.sh

--- zsh/zsh_install.sh
+++ zsh/zsh_install.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
 if ! type zsh &> /dev/null; then
-  sudo apt install zsh
+  sudo apt install -y zsh
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-if [ ! -e "$HOME/.zshrc" ] ; then
-  ln -sf "$DOTFILES_DIR/zshrc" $HOME/.zshrc
-  source $HOME/.zshrc
-fi
+rm $HOME/.zshrc
+ln -sf "$DOTFILES_DIR/zshrc" $HOME/.zshrc
+source $HOME/.zshrc
 
-if [ ! -e "$HOME/.zshenv" ] ; then
-  ln -sf "$DOTFILES_DIR/zshenv" $HOME/.zshenv
-  source $HOME/.zshenv
-fi
+rm $HOME/.zshenv
+ln -sf "$DOTFILES_DIR/zshenv" $HOME/.zshenv
+source $HOME/.zshenv
+
