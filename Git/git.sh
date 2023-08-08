0c8ae77bb543fc369737fcc51f2eae88515c9a8c
Youngseok <youngseok.choi@kaist.ac.kr>
2023-08-08 06:07:14 +0000

    Create Git configuration script

--- Git/git.sh
+++
@@ -1,8 +0,0 @@
-git config --global core.editor vim # set default editor as vim
-
-# set global user email and name
-read -p "Enter your email: " email
-read -p "Enter your name: " name
-
-git config --global user.email $email
-git config --global user.name $name
