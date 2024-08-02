247a2944dd9f60933221254bfa0eafaf8c1354f0
Youngseok Choi <youngseok.choi@kaist.ac.kr>
2024-08-02 13:33:47 +0900

    Remove bat

--- bat/run.sh
+++
@@ -1,9 +0,0 @@
-if [ type bat &> /dev/null ] || [ type batcat &> /dev/null ]
-then
-  apt-get install bat
-fi
-
-if [ type batcat &> /dev/null ] && [ ! type bat &> /dev/null ]
-then
-  ln -s /usr/bin/batcat ~/.local/bin/bat
-fi
