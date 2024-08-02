6266b26f97699f596f3c9b7d5631d2f748da228d
Youngseok <youngseok.choi@kaist.ac.kr>
2024-08-02 13:42:35 +0900

    Remove NeoVim scripts

--- NeoVim/lua/lsp-config.lua
+++
@@ -1,3 +0,0 @@
-require'lspconfig'.denols.setup{}
-
-vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]]
--- NeoVim/lua/plugins.lua
+++
@@ -1,9 +0,0 @@
-local Plug = vim.fn['plug#']
-
-vim.call('plug#begin', '~/.config/nvim-plugged')
-
-Plug 'dracula/vim'
-Plug 'neovim/nvim-lspconfig'
-
-vim.call('plug#end')
-
